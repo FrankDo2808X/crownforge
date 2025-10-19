c6ca2da6ddf21f04c6e3edcda45eab191dacdbee
Amarendra singh <110802431+FrankDo2808X@users.noreply.github.com>
2025-10-19 15:48:22 +0530

    Add files via upload

---
+++ hackersfinds.js
@@ -0,0 +1,96 @@
+PublicKeyCredential()
+PushManager()
+WebTransportBidirectionalStream();
+
+hackerfinder()
+{
+    if (index.html) {
+        true==hackerfinder
+    }
+    try {
+        
+
+    } catch (name) {"form windows api==account.tranform=-+= account name"
+        TimeRanges("")=clearTimeout("")==name;
+        DataView==name;
+    }
+}
+
+//hacker list
+hacker=("Rahul Raj!, email = not found!"(time("3:00 Am")) ,(Date("10/10/2025")))
+hacker2=("Raj Parmar, email = not found! "(time("9:50 Am")) ,(Date("14/10/2025")))
+
+banlist()
+{
+    if (ip==false) 
+    {
+         //ban list
+    hacker=-(btoa.index.html);
+    hacker2=-(btoa.index.html);
+    close==ip,(console.error("CONSLE:- Your account has been Banded for You are Trying to Attacking our site!")
+    )    
+    }
+}
+
+//protection level of Website
+
+WebTransportDatagramDuplexStream.WebTransportBidirectionalStream==(AuthenticatorAssertionResponse);
+("protection level was 32PL")
+
+//Max Protection
+
+location=loingandsigupfuction.html
+id = password
+password== null,Number,SVGAnimatedNumber,SVGNumberList;
+
+if (location=id+password) 
+{
+    //ip:crownforgeapi90L89#34459studio909s
+    //ip port:598990
+
+    ip==("any ip with port and password , id , and loction");
+    location=ip("")
+    id=password("")
+    password==ip("");    
+    Gmail=ip+Mailfounder("");
+}
+
+//mail founder
+
+Mailfounder()
+{
+    if (ip=false) 
+    {
+        Mailfounder=WebGLSampler==ip;    
+    }
+}
+
+//DEVLOPERS
+
+devlopersAntiBan()
+{
+    if (Mailfounder=-devlopersAntiBan(false=-true)) 
+    {
+        mail-1("abhigamer0338@gmail.com");
+        mail-2("");
+        mail-3("");
+        mail-4("");
+        mail-5("");
+        mail-6("");
+        mail-7("");
+        mail-8("");
+        mail-9("");
+        mail-10("");
+        mail-11("");
+        mail-12("");
+        mail-13("");
+        mail-14("");
+        mail-15("");
+        mail-16("");
+        mail-17("");
+        mail-18("");
+        mail-19("");
+        mail-20("");
+    }
+
+}
