2b9451f1bffc50e615324aea16c9fd93dd45576c
Amarendra singh <110802431+FrankDo2808X@users.noreply.github.com>
2025-10-29 10:45:00 +0530

    Add files via upload

---
+++ serverbot.js
@@ -0,0 +1,50 @@
+//Pubilc and private Function For Bot!...
+
+link("serverbot.py");
+
+//Bot Connecting... Program.
+
+linkbot()
+{
+    if (InputDeviceInfo  ("Device-info")) 
+    {
+        Device-info==DeviceMotionEvent(true);
+    }
+    else
+    {
+        Device-info==-DeviceMotionEvent(false);
+    }
+    RTCRtpSender()
+    {
+        if (DeviceMotionEvent=true) 
+        {
+            ("serverbot.py")=("BotNotFound!");    
+        }
+        else
+        {
+            DeviceMotionEvent=false
+            ("serverbot.py")=("BotFinded!");
+        }
+    }
+}
+
+//Console for Bot To work and functions!
+
+linkbotconsole()
+{
+    link==Device-info("Bot=True");
+    linkbot=-DecompressionStream();
+
+    print("Bot Has Been Started To Transfer you to Any links! With Safely!");
+    if (print==true) 
+    {
+        linkbotconsole=true;
+        serverbot.py=true
+    }
+    else
+    {
+        print=false;
+        linkbotconsole=false,print.prototype("Bot Has not Connected To Server Please Try Agin!");
+        serverbot.py==false;
+    }
+}
---
+++ serverbot.py
@@ -0,0 +1,4 @@
+link=("server.js")=True;
+print("Bot has been Connected!");
+link=("server.js")=False;
+print("Bot is not Connected Please try Agin Next Time!");
